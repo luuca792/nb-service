ea5b955aa527c5d1aaa24e1d3ff647385a5e3240
Minh Bang <bradonleminhbang1@gmail.com>
2023-11-26 15:18:21 +0700

    NB_0005: Fixing mapper of fields to list all its values

--- nb-domain/src/main/java/ctu/se/oda/model11/models/queries/fields/RetrieveFieldQueryResDTO.java
+++ nb-domain/src/main/java/ctu/se/oda/model11/models/queries/fields/RetrieveFieldQueryResDTO.java
@@ -3,10 +3,13 @@ package ctu.se.oda.model11.models.queries.fields;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import ctu.se.oda.model11.models.queries.fieldtypes.RetrieveFieldTypeQueryResDTO;
 import ctu.se.oda.model11.models.queries.notebooks.RetrieveNotebookQueryResDTO;
+import ctu.se.oda.model11.models.queries.values.RetrieveValueQueryResDTO;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Data
 @Builder
@@ -21,4 +24,6 @@ public class RetrieveFieldQueryResDTO {
     private RetrieveNotebookQueryResDTO notebook;
     @JsonProperty("fieldType")
     private RetrieveFieldTypeQueryResDTO fieldType;
+    @JsonProperty("fieldValues")
+    private List<RetrieveValueQueryResDTO> values;
 }
